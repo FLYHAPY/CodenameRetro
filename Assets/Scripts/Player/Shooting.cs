94ea7a2ea8f8fbf454f8743c7fae905778addcb2
FLYHAPY <eduardospinola04@gmail.com>
2026-01-31 18:02:59 +0000

    added the ability to shoot

--- Assets/Scripts/Player/Shooting.cs
+++ Assets/Scripts/Player/Shooting.cs
@@ -4,10 +4,10 @@ using UnityEngine.InputSystem;
 public class Shooting : MonoBehaviour
 {
     public InputActionReference playerFire;
-    public InputActionReference playerMOve;
     public bool isShooting;
     public GameObject bullet;
-    public float speed;
+    public float shootForce = 10f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,8 +17,17 @@ public class Shooting : MonoBehaviour
 
     private void Fire(InputAction.CallbackContext obj)
     {
-        GameObject bulletInstance = Instantiate(bullet, gameObject.transform.position, gameObject.transform.rotation);
-        bulletInstance.GetComponent<Rigidbody2D>().linearVelocity = speed * playerMOve.action.ReadValue<Vector2>();
+        Vector3 mouseScreenPos = Mouse.current.position.ReadValue();
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+        mouseWorldPos.z = 0f;
+        
+        Vector2 direction = (mouseWorldPos - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 180f;
+        
+        GameObject ball = Instantiate(bullet, transform.position, Quaternion.Euler(0f, 0f, angle));
+        
+        Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+        rb.AddForce(direction * shootForce, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
