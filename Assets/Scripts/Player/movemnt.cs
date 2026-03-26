15b42379ac24a86e1f884a27398cce920eb50150
FLYHAPY <eduardospinola04@gmail.com>
2026-03-26 13:11:49 +0000

    added running animation

--- Assets/Scripts/Player/movemnt.cs
+++ Assets/Scripts/Player/movemnt.cs
@@ -5,20 +5,25 @@ public class movemnt : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed;
-    public InputActionReference playercontrols;
-    private Vector2 moveDiretion;
+    public InputActionReference playerControls;
+    private Vector2 _moveDirection;
+
+    [SerializeField]
+    private Animator anim;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playercontrols.action.Enable();
+        playerControls.action.Enable();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.linearVelocity = moveDiretion * speed;
-        moveDiretion = playercontrols.action.ReadValue<Vector2>();
+        rb.linearVelocity = _moveDirection * speed;
+        _moveDirection = playerControls.action.ReadValue<Vector2>();
+        anim.SetFloat("MoveX", _moveDirection.x);
+        anim.SetFloat("MoveY", _moveDirection.y);
     }
 }
